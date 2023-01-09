03fa6a389a7e6debc22fa7228cea0bb49a38f755
Sefa <sfatektas55@gmail.com>
2023-01-09 22:29:35 +0300

    Uow Created

--- denemeBlazor/Data/Repository/Repository{T}.cs
+++ denemeBlazor/Data/Repository/Repository{T}.cs
@@ -1,8 +1,16 @@
-﻿using SportsStore.Data.Entities;
+﻿using SportsStore.Data.Context;
+using SportsStore.Data.Entities;
 
 namespace denemeBlazor.Data.Repository
 {
     public class Repository<T> where T : BaseEntity
     {
+        readonly NewsDbContext _context;
+
+        public Repository(NewsDbContext context)
+        {
+            _context = context;
+        }
+
     }
 }
