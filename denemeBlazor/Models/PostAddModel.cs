8fa01159f26092bf29ea1cd8b091d598c75897d3
Sefa <sfatektas55@gmail.com>
2023-01-17 00:32:40 +0300

    gititgnore added

--- denemeBlazor/Models/PostAddModel.cs
+++ denemeBlazor/Models/PostAddModel.cs
@@ -1,7 +1,19 @@
-﻿namespace denemeBlazor.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace denemeBlazor.Models
 {
     public class PostAddModel
     {
+        public string Title { get; set; }
 
+        public string Defination1 { get; set; }
+
+        public string Defination2 { get; set; }
+
+        public IFormFile MediaPath { get; set; }
+
+        public SelectList CategorySelectList { get; set; }
+
+        public int AppUserId { get; set; }
     }
 }
