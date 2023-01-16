61f8dbdc73687b93e2651663bc3b6480f0e81cb7
Murat Baran <mrtbrn911@gmail.com>
2023-01-17 01:29:04 +0300

    Bolder Tag Helper projeye eklendi

---
+++ denemeBlazor/TagHelpers/StrongTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace denemeBlazor.TagHelpers
+{
+    [HtmlTargetElement("bolderTagHelper")]
+    public class StrongTagHelper : TagHelper
+    {
+        public string kullaniciAdi { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"<b>{kullaniciAdi}<b>"); 
+        }
+    }
+
+}
+
