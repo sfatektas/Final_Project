c30bedf10cc87be0942f6c4812ce0a7426081e70
Murat Baran <mrtbrn911@gmail.com>
2023-01-16 23:49:16 +0300

    Comment ViewComponent Oluşturuldu Comment Service custom method eklendi

---
+++ denemeBlazor/ViewComponents/GetCommands.cs
@@ -0,0 +1,23 @@
+﻿using denemeBlazor.Bussines.Dtos;
+using denemeBlazor.Bussines.Interfaces;
+using denemeBlazor.Bussines.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace denemeBlazor.ViewComponents
+{
+    public class GetCommands : ViewComponent
+    {
+        private readonly ICommentService _commentService;
+        public GetCommands(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var response =await _commentService.GetNewsAllCommantsWithPostId(id);
+            return View(response.Data);
+        }
+    }
+}
